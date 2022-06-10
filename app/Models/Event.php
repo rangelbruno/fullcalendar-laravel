868ca369cd2c51c0ebf40b9b560dcfc60be1f271
rangelbruno <brunojp.rangel@gmail.com>
2022-06-09 22:54:37 -0300

    Date Attributes

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,19 @@ class Event extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'start', 'end', 'color'];
+
+    public function getStartAttribute($value)
+    {
+        $dateStart = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('Y-m-d');
+        $timeStart = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('H:i:s');
+
+        return $this->start = ($timeStart == '00:00:00' ? $dateStart : $value);
+    }
+    public function getEndAttribute($value)
+    {
+        $dateEnd = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('Y-m-d');
+        $timeEnd = Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('H:i:s');
+
+        return $this->end = ($timeEnd == '00:00:00' ? $dateEnd : $value);
+    }
 }
